6be2ac31629d801e73e773f2cadf7781570e6796
Xardohr <xardohr@gmail.com>
2023-04-10 12:32:30 +0300

    changed api from http to https and changed number colors

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const celciusBtn = document.querySelector(".celcius-button");
 async function getWeather(city) {
   try {
     const response = await fetch(
-      `http://api.weatherapi.com/v1/current.json?key=d46d34a0468f4667b3d143437233003&q=${city}&aqi=no`,
+      `https://api.weatherapi.com/v1/current.json?key=d46d34a0468f4667b3d143437233003&q=${city}&aqi=no`,
       { mode: "cors" }
     );
     const weatherData = await response.json();
